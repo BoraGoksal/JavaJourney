690b593f8024bc1002471535e180b4d8735dd2bc
boragoksal <boragoksal@outlook.com>
2025-02-11 23:53:33 +0000

    Sorted and Removed duplicates from Array Basic Exercises

--- DataStructures_ArrayList.java
+++ DataStructures_ArrayList.java
@@ -1,6 +1,5 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
+
 
 public class DataStructures_ArrayList {
 
@@ -8,18 +7,33 @@ public class DataStructures_ArrayList {
     public static void main(String[] args){
         //1.
         ArrayList<Integer> Numberlist = new ArrayList<>();
-        addRemove(Numberlist,3);
+        AddRemove(Numberlist,3);
         PrintNumbers(Numberlist);
 
-        //2.
+        //2. Search Array (Contains())
         String[] FruitArray = {"Banana", "Apple", "Strawberry", "Blackberry", "Watermelon"};
         ArrayList<String> FruitList = new ArrayList<>(Arrays.asList(FruitArray));
         String searchItem = "Apple";
-        searchList(FruitList, searchItem);
+        SearchArrayList(FruitList, searchItem);
+
+        //3. Sorted Array (Collections.sort())
+        ArrayList<String> Words = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        SortArrayList(Words, scanner);
+
+        //4. Remove Duplicates(Hashset<>,Clear(),addAll())
+        ArrayList<Integer> duplicateNums = new ArrayList<>();
+        duplicateNums.add(50);
+        duplicateNums.add(20);
+        duplicateNums.add(30);
+        duplicateNums.add(20);
+        duplicateNums.add(35);
+        duplicateNums.add(35);
+        RemoveDuplicate(duplicateNums);
     }
     //1. Basic Operations with ArrayList (adding and removing elements)
-    static void addRemove(ArrayList<Integer> NumberList, int j){
-        for(int i = 1; i <= 5; i++) {
+    public static void AddRemove(ArrayList<Integer> NumberList, int j){
+        for(int i = 1; i <= 4; i++) {
             NumberList.add(i);
             if(j != -1 && NumberList.size() == 5){
                 NumberList.remove(j);
@@ -27,16 +41,32 @@ public class DataStructures_ArrayList {
         }
     }
     //2. Searching in an ArrayList
-    static void searchList(ArrayList<String> StringList, String searchItem){
+    public static void SearchArrayList(ArrayList<String> StringList, String searchItem){
             if(StringList.contains(searchItem)){
                 System.out.println("The List contains:" + searchItem);
             } else {
                 System.out.println("The List does NOT contain:" + searchItem);
             }
     }
-    static void PrintNumbers(ArrayList<Integer> List){
+    public static void PrintNumbers(ArrayList<Integer> List){
         for(Integer i : List){
             System.out.println(i);
         }
+    }
+    public static void SortArrayList(ArrayList<String> List, Scanner scanner){
+        System.out.println("Type 5 words:");
+        for(int i = 0; i < 5; i++){
+            List.add(scanner.nextLine());
+        }
+        Collections.sort(List);
+        System.out.println("Sorted List: " + List);
+    }
+    public static void RemoveDuplicate(ArrayList<Integer> List){
+        //Hashsets don't allow duplicates and has O(1) time complexity
+        HashSet<Integer> uniqueNumbers = new HashSet<>(List);
+        List.clear();
+        List.addAll(uniqueNumbers);
+        Collections.sort(List);
+        System.out.println("List after removing duplicates and sorting: " + List);
     }
 }
